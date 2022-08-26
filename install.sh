ee1bf384a0407cb626013025d66e5ebc3a6305fd
luckYrat <lacklat.key@gmail.com>
2022-08-26 10:09:52 +0900

    fix(installer): exclude .git

--- install.sh
+++ install.sh
@@ -7,13 +7,12 @@ cd $BASEDIR
 
 IFS=$'\n'
 
-for f in `find . -type d -prune -o -type f -print`; do
+for f in `find . -type d -o -type f -print`; do
     [[ $f =~ ".md" ]] && continue
     [[ $f =~ "install.sh" ]] && continue
     [[ $f =~ ".swp" ]] && continue
-    [ "$f" = ".git" ] && continue
+    [[ $f =~ ".git" ]] && continue
     # if folder does not exist, make folder
     mkdir -p ~/"$(dirname $f)"
     ln -snfv ${PWD}/"$f" ~/"$f"
 done
-
