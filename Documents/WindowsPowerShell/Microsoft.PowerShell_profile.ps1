d1e3b8156b37beb111766ebf1df87cb1d66e19e4
luckylat <lacklat.key@gmail.com>
2023-03-07 13:45:16 +0900

    feat(zoxide): setup

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,10 @@
 Invoke-Expression (&starship init powershell)
 
+Invoke-Expression (& {
+    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+    (zoxide init --hook $hook powershell | Out-String)
+})
+
 $Env:SHELL = 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe'
 
 set-alias vim 'C:\Program Files\Neovim\bin\nvim.exe'
