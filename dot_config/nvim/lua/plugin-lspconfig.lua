8412c472810ae2ae73a1cfa58e8f7958b48224a6
luckylat <lacklat.key@gmail.com>
2023-06-04 02:11:49 +0900

    feat(nvim): add lsp shortcut setting

--- dot_config/nvim/lua/plugin-lspconfig.lua
+++ dot_config/nvim/lua/plugin-lspconfig.lua
@@ -3,13 +3,41 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local lspconfig = require('lspconfig')
 
+-- TypeScript
 lspconfig['tsserver'].setup {
     capabilities = capabilities
 }
+
+-- Rust
 lspconfig['rust_analyzer'].setup {
     capabilities = capabilities,
     settings = {
         ['rust-analyzer'] = {},
     },
 }
+
+lspconfig['ccls'].setup {
+    capabilities = capabilities,
+    init_options = {
+        cache = {
+            directory = ".ccls-cache";
+        };
+    }
+}
+
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+map('n', '<space>e', 'vim.diagnostic.open_float', opts)
+map('n', '[d', 'vim.diagnostic.goto_prev', opts)
+map('n', ']d', 'vim.diagnostic.goto_next', opts)
+
+local on_attach = function(client, bufnr)
+    local bufopts = { noremap=true, silent=true, buffer=bufnr }
+    map('n', 'K', vim.lsp.buf.hover, bufopts)
+    map('n', 'gd', vim.lsp.buf.definition, bufopts)
+    map('n', 'gi', vim.lsp.buf.implemention, bufopts)
+    map('n', 'gr', vim.lsp.buf.references, bufopts)
+    map('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+end
 
