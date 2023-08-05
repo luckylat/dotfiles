800429c614926793209bc9522b443841d09296d2
luckylat <lacklat.key@gmail.com>
2023-08-05 11:25:28 +0900

    feat(nvim): set treesitter fold method

--- dot_config/nvim/lua/custom-012-normalCoding.lua
+++ dot_config/nvim/lua/custom-012-normalCoding.lua
@@ -1,13 +1,7 @@
-vim.opt.foldmethod = 'syntax'
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.foldenable = false
 vim.opt.foldlevel = 100
-
-vim.api.nvim_create_autocmd({'BufEnter', 'BufAdd', 'BufNew', 'BufNewFile', 'BufWinEnter'}, {
-    group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
-    callback = function()
-        vim.opt.foldmethod = 'expr'
-        vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
-    end
-})
 
 vim.api.nvim_create_augroup('folding', { clear = true })
 vim.api.nvim_create_autocmd('BufWritePost', {
@@ -25,5 +19,3 @@ vim.api.nvim_create_autocmd('FileType', {
     pattern = { 'tex', 'lua' },
     callback = function() vim.opt_local.foldmethod = 'indent' end
 })
-
-
