afb204d74b6941eb3bcb095185b9b5ddbfc98898
luckylat <lacklat.key@gmail.com>
2023-09-23 14:30:38 +0900

    show foldmethod on bottom

--- dot_config/nvim/lua/plugin-lualine.lua
+++ dot_config/nvim/lua/plugin-lualine.lua
@@ -1,6 +1,12 @@
+
+local function getFoldmethod()
+    return vim.opt.foldmethod:get()
+end
+
 require('lualine').setup {
     options = { theme = 'codedark' },
     sections = {
-        lualine_c = {{'filename', path=1}}
+        lualine_c = {{'filename', path=1}},
+        lualine_y = {getFoldmethod}
     }
 }
